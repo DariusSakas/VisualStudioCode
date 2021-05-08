65f95c6696f846a880eb14ddb4b1a3341bf53bff
Darius <sakas.darius@gmail.com>
2021-05-08 12:02:02 +0300

    Added new functionality

--- TypeScript/exercise1.js
+++ TypeScript/exercise1.js
@@ -1,2 +1,17 @@
 var count = 5;
 console.log(count);
+var a;
+var b;
+var s;
+var any;
+var m = [1, 2, 3];
+var anyType = [2, "hi", "privet"];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Blue"] = 1] = "Blue";
+    Color[Color["Green"] = 2] = "Green";
+})(Color || (Color = {}));
+var message = "abs";
+var newMessage = message.endsWith("s");
+console.log(newMessage);
--- TypeScript/exercise1.ts
+++ TypeScript/exercise1.ts
@@ -1,2 +1,25 @@
 let count = 5;
 console.log(count);
+
+let a: number;
+
+let b: boolean;
+
+let s: string;
+
+let any: any;
+
+let m: number[] = [1, 2, 3];
+
+let anyType: any[] = [2, "hi", "privet"];
+
+enum Color {
+  Red,
+  Blue,
+  Green,
+}
+
+let message = "abs";
+
+let newMessage = message.endsWith("s");
+console.log(newMessage);
