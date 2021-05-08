1f135e0cea45017e30fdfb0cf1c288bc5924a095
Darius <sakas.darius@gmail.com>
2021-05-08 11:03:48 +0300

    a

---
+++ JavaScript/first.js
@@ -0,0 +1,2 @@
+console.log(`Hello from seperate file!`);
+alert("Hi.");
---
+++ JavaScript/variables.js
@@ -0,0 +1,11 @@
+let topic = "Variables topic";
+console.log("Topic's name is " + topic);
+console.log(`Topic's name is ${topic}`);
+
+let person = {
+  name: "Mario",
+  age: 25,
+};
+
+console.log(person);
+console.log(person["age"]);
